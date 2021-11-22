364bb330f07887d1bcafcbe9d6f271df21ab936c
romain <romainmichel@ynov.com>
2021-11-22 19:20:11 +0100

    ptn

--- api/app/models/index.js
+++ api/app/models/index.js
@@ -2,11 +2,18 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const MONGO_URL = process.env;
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB,
+    MONGO_URL
+} = process.env;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = MONGO_URL
+db.url = `${MONGO_URL}`
 db.events = require("./event.model.js")(mongoose);
 
 module.exports = db;
