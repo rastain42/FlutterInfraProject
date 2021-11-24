9c2f46b7472ec8ddd59441d8ade9a6d71a856560
romain <romainmichel@ynov.com>
2021-11-24 14:22:16 +0100

    mmhhhhhh

--- api_node-mongo/nodeApp/app/models/index.js
+++ api_node-mongo/nodeApp/app/models/index.js
@@ -5,6 +5,6 @@ const dbConfig = require("../config/db.config.js");
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.events = require("./event.model.js")(mongoose);
+db.events = require("./events.model.js")(mongoose);
 
 module.exports = db;
